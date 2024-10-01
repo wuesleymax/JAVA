bf1db2cacc8debaf99d8d2e457e9ebbf0c898a40
wuesleymax <wuesleysoaresmaximiano@gmail.com>
2024-09-30 21:14:51 -0300

    new1

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
+
 make: build run
 
 build:
---
+++ aulas/Aula30.java
@@ -0,0 +1,17 @@
+package aulas;
+import libs.Matrizes;
+
+
+
+public class Aula30 {
+    public static void aula(){
+        System.out.println("Aula30");
+    }
+
+
+    public static void ex01(){
+        
+    }
+
+    
+}
