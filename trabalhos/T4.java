0d11425d3dbaf5ee491e0fc71e90fe5a1382e84e
wuesleymax <wuesleysoaresmaximiano@gmail.com>
2024-12-16 20:54:07 -0300

    16--12--2024

--- trabalhos/T4.java
+++ trabalhos/T4.java
@@ -11,106 +11,65 @@ public class T4 {
         TesteQuick();
         TesteRadix();
         TesteSeletion();
-        TesteBubblesort();
+        // TesteBubblesort();
         TesteMerge();
         TesteInsertion();
-
-    
     }
 
     public static void TesteBubblesort() {
-        // Gera um vetor com 60.000 elementos aleatórios
         int[] v = Vetores.gerarValoresAleatorios(1000000, 0, 10000000);
-        
-        // Medir o tempo de execução da ordenação (Insertion Sort)
         long tempo_inicio_ordenacao = Tempo.medirTempoIniciar();
-        Vetores.bubbleSort(v); // Chama a função de ordenação
+        Vetores.bubbleSort(v);
         double tempo_ordenacao_segundos = Tempo.medirTempoFinalizarSegundos(tempo_inicio_ordenacao);
-    
-        // Exibir o tempo de execução da ordenação
-        System.out.printf("Tempo de ordenação (bubble Sort) com %d elementos: %f segundos\n", v.length, tempo_ordenacao_segundos);
+        System.out.println("Tempo de ordenação (bubble Sort) com " + v.length + " elementos: " + tempo_ordenacao_segundos + " segundos");
     }
 
-
     public static void TesteInsertion() {
-        // Gera um vetor com 60.000 elementos aleatórios
         int[] v = Vetores.gerarValoresAleatorios(100000, 0, 10000000);
-        
-        // Medir o tempo de execução da ordenação (Insertion Sort)
         long tempo_inicio_ordenacao = Tempo.medirTempoIniciar();
-        Vetores.ordenarInsertionSort(v); // Chama a função de ordenação
+        Vetores.ordenarInsertionSort(v);
         double tempo_ordenacao_segundos = Tempo.medirTempoFinalizarSegundos(tempo_inicio_ordenacao);
-    
-        // Exibir o tempo de execução da ordenação
-        System.out.printf("Tempo de ordenação (Insertion Sort) com %d elementos: %f segundos\n", v.length, tempo_ordenacao_segundos);
+        System.out.println("Tempo de ordenação (Insertion Sort) com " + v.length + " elementos: " + tempo_ordenacao_segundos + " segundos");
     }
 
     public static void TesteSeletion() {
-        // Gera um vetor com 60.000 elementos aleatórios
         int[] v = Vetores.gerarValoresAleatorios(100000, 0, 10000000);
-        
-        // Medir o tempo de execução da ordenação (Insertion Sort)
         long tempo_inicio_ordenacao = Tempo.medirTempoIniciar();
-        Vetores.ordenarSelectionSort(v); // Chama a função de ordenação
+        Vetores.ordenarSelectionSort(v);
         double tempo_ordenacao_segundos = Tempo.medirTempoFinalizarSegundos(tempo_inicio_ordenacao);
-    
-        // Exibir o tempo de execução da ordenação
-        System.out.printf("Tempo de ordenação (Seletion Sort) com %d elementos: %f segundos\n", v.length, tempo_ordenacao_segundos);
+        System.out.println("Tempo de ordenação (Seletion Sort) com " + v.length + " elementos: " + tempo_ordenacao_segundos + " segundos");
     }
 
     public static void TesteMerge() {
-        // Gera um vetor com 60.000 elementos aleatórios
         int[] v = Vetores.gerarValoresAleatorios(100000, 0, 10000000);
-        
-        // Medir o tempo de execução da ordenação (Insertion Sort)
         long tempo_inicio_ordenacao = Tempo.medirTempoIniciar();
-        Vetores.mergeSort(v); // Chama a função de ordenação
+        Vetores.mergeSort(v);
         double tempo_ordenacao_segundos = Tempo.medirTempoFinalizarSegundos(tempo_inicio_ordenacao);
-    
-        // Exibir o tempo de execução da ordenação
-        System.out.printf("Tempo de ordenação (Merge Sort) com %d elementos: %f segundos\n", v.length, tempo_ordenacao_segundos);
+        System.out.println("Tempo de ordenação (Merge Sort) com " + v.length + " elementos: " + tempo_ordenacao_segundos + " segundos");
     }
 
     public static void TesteCounting() {
-        // Gera um vetor com 60.000 elementos aleatórios
         int[] v = Vetores.gerarValoresAleatorios(100000, 0, 100000);
-        
-        // Medir o tempo de execução da ordenação (Insertion Sort)
         long tempo_inicio_ordenacao = Tempo.medirTempoIniciar();
-        Vetores.CountingSort(v ,1); // Chama a função de ordenação
+        Vetores.CountingSort(v ,1);
         double tempo_ordenacao_segundos = Tempo.medirTempoFinalizarSegundos(tempo_inicio_ordenacao);
-    
-        // Exibir o tempo de execução da ordenação
-        System.out.printf("Tempo de ordenação (CountingSort) com %d elementos: %f segundos\n", v.length, tempo_ordenacao_segundos);
+        System.out.println("Tempo de ordenação (CountingSort) com " + v.length + " elementos: " + tempo_ordenacao_segundos + " segundos");
     }
 
     public static void TesteQuick() {
-        // Gera um vetor com 60.000 elementos aleatórios
         int[] v = Vetores.gerarValoresAleatorios(100000, 0, 100000);
-        
-        // Medir o tempo de execução da ordenação (Insertion Sort)
         long tempo_inicio_ordenacao = Tempo.medirTempoIniciar();
-        Vetores.quickS(v); // Chama a função de ordenação
+        Vetores.quickS(v);
         double tempo_ordenacao_segundos = Tempo.medirTempoFinalizarSegundos(tempo_inicio_ordenacao);
-    
-        // Exibir o tempo de execução da ordenação
-        System.out.printf("Tempo de ordenação (QuickSort) com %d elementos: %f segundos\n", v.length, tempo_ordenacao_segundos);
+        System.out.println("Tempo de ordenação (QuickSort) com " + v.length + " elementos: " + tempo_ordenacao_segundos + " segundos");
     }
 
     public static void TesteRadix() {
-        // Gera um vetor com 60.000 elementos aleatórios
         int[] v = Vetores.gerarValoresAleatorios(100000, 0, 100000);
-        
-        // Medir o tempo de execução da ordenação (Insertion Sort)
         long tempo_inicio_ordenacao = Tempo.medirTempoIniciar();
-        Vetores.radixSort(v); // Chama a função de ordenação
+        Vetores.radixSort(v);
         double tempo_ordenacao_segundos = Tempo.medirTempoFinalizarSegundos(tempo_inicio_ordenacao);
-    
-        // Exibir o tempo de execução da ordenação
-        System.out.printf("Tempo de ordenação (radixSort) com %d elementos: %f segundos\n", v.length, tempo_ordenacao_segundos);
+        System.out.println("Tempo de ordenação (radixSort) com " + v.length + " elementos: " + tempo_ordenacao_segundos + " segundos");
     }
-
-
-
-    
 }
+
